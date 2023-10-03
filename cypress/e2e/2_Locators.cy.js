bee6859f277358420aa93db394cbb1b7c3d94952
Ersin KAYHAN <dekked2438@gmail.com>
2023-10-03 17:59:49 +0300

    add: Locators continue

--- cypress/e2e/2_Locators.cy.js
+++ cypress/e2e/2_Locators.cy.js
@@ -14,7 +14,7 @@ describe('Find or Get elements by using different locators', () => {
 
         // 1_ By using CSS locator, cypress needs additional dependency to use Xpath
 
-        cy.get("input[name='username']").type('CydeoStudent');
+        cy.get("input[name='username']").type("CydeoStudent");
         /**  every statement creates an object to be interacted, and next command makes operation on the object
          * that has been created at the previous statement.
          */ 
@@ -25,8 +25,18 @@ describe('Find or Get elements by using different locators', () => {
         // it again using attribute and value.
 
 
+        // 3_ How to locate multiple elements
+        //We have 2 elements having input as a tag name. to handle it, we can use each function. 
+        // It has 3 parameters, item, index, and the list. We can change the names in function.
+        /** 
+        cy.get("input").each((item,index,list1), ()=>{
 
+            // Assert the length of the list is 2.
+            expect(list1).to.have.length(2);
+            expect(item).to.have.attr("type");
+
+        })
+*/
     })
-
 
 })

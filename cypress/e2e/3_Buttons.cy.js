8a821da3baffb0a6bc8da176c21bc57b0d4dec90
Ersin KAYHAN <dekked2438@gmail.com>
2023-10-04 21:54:27 +0300

    Added: eslint scripts

--- cypress/e2e/3_Buttons.cy.js
+++ cypress/e2e/3_Buttons.cy.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+
+
 describe('My Second Test', () => {
 
     beforeEach(() => {
@@ -26,10 +28,32 @@ describe('My Second Test', () => {
 
         })
 
+        // get all buttons using button tag name, each method is creating a loop.
+        cy.get('button').each((item, index, list) => {
+            // assert the length of the list, verfy the number of buttons
+            expect(list).to.have.length(6);
+            expect(item).to.have.attr('onclick');
+        })
+
+        /** We'll get all the buttons like previous approach, get only the item then check of 
+         * text of each item, if it eaquals to button 4, then click on it.
+        */
+
+        cy.get('button').each((item)=>{
+
+            if (item.text()=='Button 4'){
+                cy.log(item.text()); // This command writes the text of button 4 to the test console
+                
+                //item.click() will work, but this method is depricated, because item is JQuerry now.
+                // We need to wrap() it, so convert it to cypress.
+                cy.wrap(item).click();
+            cy.contains('Clicked on button four!').should('be.visible');
+            }
+            
+
+
+
+        })
 
     })
-
-
-
-
 })
